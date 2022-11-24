11ec0a576925413851b5cee6ef59609d1058ce1d
salesleticia <116445652+salesleticia@users.noreply.github.com>
2022-11-23 21:01:50 -0300

    23/11

---
+++ BARBEARIA/listaluno.js
@@ -0,0 +1,34 @@
+let aluno = 'Jamir'
+
+switch(aluno){
+    case ' letícia':
+    console.log('letícia tem 17 anos')
+    break
+    case ' yuri':
+    console.log(' yuri tem 18 anos')
+    break
+    case ' evandro':
+    console.log(' evandro tem 16 anos')
+    break
+    case ' maria eduarda':
+    console.log('maria eduarda tem 16 anos')
+    break
+    case  'mariany':
+    console.log(' mariany tem 15 anos')
+    break
+    case 'nilmara':
+    console.log('nilmara tem 19 anos')
+    break
+    case 'cauã':
+    console.log('cauã tem 16 anos')
+    break
+    case 'jamir':
+    console.log('jamir tem 20 anos')
+    break
+    case 'talita':
+    console.log('talita tem 21 anos')
+    break
+    case ' emilly':
+    console.log('emilly tem 19 anos')
+    break
+}
